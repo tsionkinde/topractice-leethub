4bb5d19f67ca24180a133f2db53260171706064c
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-10-29 11:38:43 -0700

    Time: 0 ms (100%), Space: 19 MB (33.81%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      num_to_index={}
+      for index,num in enumerate(nums):
+        if target-num in num_to_index:
+            return [num_to_index[target-num],index]
+        num_to_index[num]=index
+      return []       
+            
