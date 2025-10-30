707d814cb82414c7ae69bc539218e69f72b1460c
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-10-30 13:10:51 -0700

    Time: 0 ms (100%), Space: 18.2 MB (70.38%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        fizz_buzz=[]
+        for i in range(1,n+1):#b/c it is 1 indexed that means the index starts from 1
+            if i%3==0 and i%5==0:
+                  fizz_buzz.append("FizzBuzz")
+            elif  i%3==0:
+                fizz_buzz.append("Fizz") 
+            elif  i%5==0:
+                fizz_buzz.append("Buzz")      
+            else:
+                  fizz_buzz.append(str(i))
+        return  fizz_buzz
+        
