7c40008645bb1ee6b5192cfc029cd9092356af3a
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-11-04 02:28:47 -0800

    Time: 0 ms (100%), Space: 17.8 MB (57.55%) - LeetHub

--- 2278-find-three-consecutive-integers-that-sum-to-a-given-number/2278-find-three-consecutive-integers-that-sum-to-a-given-number.py
+++ 2278-find-three-consecutive-integers-that-sum-to-a-given-number/2278-find-three-consecutive-integers-that-sum-to-a-given-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def sumOfThree(self, num: int) -> List[int]:
-        if num%3!=0:
-            return []
+        if num%3!=0:#x-1+x+x+1=3x=num->x=num//3
+            return []#represents there is 3 no consecutive element that sum gives equal num
         x=num//3
         return [x-1,x,x+1]
       
