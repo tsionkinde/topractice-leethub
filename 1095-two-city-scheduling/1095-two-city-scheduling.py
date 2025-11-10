477c3651fedfb0cdaf12a16cd5acd4aac23e99f9
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-11-10 11:10:25 -0800

    Time: 0 ms (100%), Space: 18 MB (26.95%) - LeetHub

---
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        sorted_cost=sorted(costs,key=lambda x:x[0]-x[1])
+        result=0
+        for i in range(len(costs)):
+            if i <len(costs)/2:
+                result+=sorted_cost[i][0]
+            else:
+                result+=sorted_cost[i][1]
+        return result        
+                    
+        
+        
