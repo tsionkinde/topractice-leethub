d8e15d1ef083e715e5cb5e847ebc2a7bc03333a6
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-11-25 09:39:13 -0800

    Time: 11 ms (99.1%), Space: 24.7 MB (67.27%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        # for i in range(len(nums)):
+        #     min_index=i
+        #     for j in range(i+1,len(nums)):
+        #         if nums[j]< nums[  min_index]:
+        #     nums[i],nums[min_index]=nums[min_index],nums[i]        
+           
+            
+        # return nums   
+        return sorted(nums)
+            
+
+                
+
+
+
+
