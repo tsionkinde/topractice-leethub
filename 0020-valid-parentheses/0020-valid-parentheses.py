7382dab9e68ffe294d4a1e6115c13df31b5ba921
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-12-07 03:19:08 -0800

    Time: 0 ms (100%), Space: 17.8 MB (58.19%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []  # Initialize a stack using a list
+        for c in s:
+            if c in ['(', '{', '[']:
+                stack.append(c)  # Push the opening bracket onto the stack
+            else:
+                # Check if the stack is empty or if the top of the stack does not match the closing bracket
+                if (not stack or 
+                    (c == ')' and stack[-1] != '(') or 
+                    (c == '}' and stack[-1] != '{') or 
+                    (c == ']' and stack[-1] != '[')):
+                    return False  # Return False if brackets don't match
+                stack.pop()  # Pop the top element from the stack if it matches
+        
+        return not stack  # Return True if the stack is empty, otherwise False
