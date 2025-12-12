e57690e24ed08c98f9a7834cbc11defe9428018b
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-12-12 11:19:16 -0800

    Time: 6 ms (85.93%), Space: 18.2 MB (55.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered_chars=''.join(char.lower() for char in s if char.isalnum())
-        return filtered_chars==filtered_chars[::-1]
+        newstr=""
+        for c in s:
+            if c.isalnum():
+                newstr += c.lower()
+        return newstr==newstr[::-1]        
+        
+      
         
