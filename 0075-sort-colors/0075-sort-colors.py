8c1524f1f431def94237f24e447a396f29aa4022
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2025-12-20 10:38:13 -0800

    Time: 0 ms (100%), Space: 17.3 MB (99.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,7 +2,7 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
         """
-        sorted_colors=nums.sort()
-        return  sorted_colors
-           
+        nums.sort()
+        
