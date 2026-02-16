9d13b26c408cfbb7ed8261e72f2102de73bb1bb9
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-02-16 00:39:38 -0800

    Time: 3 ms (46.07%), Space: 23.7 MB (17.38%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -6,11 +6,12 @@ class Solution:
         """
         left=0
         right=len(s)-1
-        while  left< right:
-             s[ left],s[right]=s[right],s[ left]
-       
-             left+=1
-             right-=1
-        return s
+        while left < right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+            
+
+
 
         
