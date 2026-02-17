46321c937023f0f37602240359e5f941d6a3b91f
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-02-17 12:48:16 -0800

    Time: 19 ms (69.4%), Space: 26.4 MB (63.16%) - LeetHub

---
+++ 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findDuplicate(self, paths):
+        content_map = {}  
+        for path in paths:
+            parts = path.split(" ")
+            folder = parts[0]
+
+          
+            for file_info in parts[1:]:
+              
+                name, content = file_info.split("(")
+
+                content = content[:-1]
+
+          
+                full_path = folder + "/" + name
+
+                if content not in content_map:
+                    content_map[content] = []
+
+                content_map[content].append(full_path)
+
+       
+        result = []
+        for file_list in content_map.values():
+            if len(file_list) > 1:
+                result.append(file_list)
+
+        return result
