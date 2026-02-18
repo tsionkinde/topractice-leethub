8c3ede7755ac8ddeb1b8fefd6e02ebc061afc77b
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-02-18 07:44:27 -0800

    Time: 236 ms (14.99%), Space: 19.6 MB (73.5%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,6 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(heights)-1):
+        for i in range(len(heights)-1):#selection sort
             max_index=i
 
             for j in range(i+1,len(heights)):
