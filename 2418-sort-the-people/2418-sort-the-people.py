1e4051a4fe41080c3f443de1ce61dd78fa326d33
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-02-18 07:58:06 -0800

    Time: 310 ms (13.41%), Space: 19.8 MB (42.31%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,6 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(1,len(heights)):#selection sort
+        for i in range(1,len(heights)):#Insertion sort
             key_h=heights[i]
             key_n=names[i]
             j=i-1
