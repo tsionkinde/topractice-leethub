4ae91a05a6a234b6958747771e064bae3d877d3e
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-02-23 22:18:55 -0800

    Time: 4 ms (3.42%), Space: 19.5 MB (8.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,23 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i, j = m - 1, n - 1
-        # Pointer for the last position of nums1
-        k = m + n - 1
-        
-        # Merge in reverse order
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
+        i=0
+        j=0
+        res=[]
+        while i <m and j<n:
+            if nums1[i]<nums2[j]:
+                res.append(nums1[i])
+                i+=1
             else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
-        
-        # If there are remaining elements in nums2, add them
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+                res.append(nums2[j]) 
+                j+=1 
          
+          
+        while i<m:
+            res.append(nums1[i]) 
+            i+=1
+        while j<n:
+            res.append(nums2[j]) 
+            j+=1 
+        for k in range(m+n):
+            nums1[k]=res[k]      
+
+
+        
