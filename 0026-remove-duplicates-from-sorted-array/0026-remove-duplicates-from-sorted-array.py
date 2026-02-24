2ea78f8aeffc7381f9da4489d42a24d253a09e6d
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-02-23 22:29:14 -0800

    Time: 0 ms (100%), Space: 20.6 MB (33.91%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,17 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-       slow=0# the first unique index
-       if not nums:
-           return 0
-       for fast in range(1,len(nums)):#making fast pointer to start from 1
-            if nums[slow]!= nums[fast]:#if not equeal means still it is unique
-                slow+=1#if it is unique increment slow pointer to the other nidex
-                nums[slow]=nums[fast]#assigning the first unique index by the next unique
-       return slow+1    # it returns the length of unique elemennt because we start slow=0 we incremetn slow by 1 to count unique element      
+        slow=0
+        if not nums:
+            return 0
+        
+        for fast in range  (1,len(nums)):
+            if nums[slow]!=nums[fast]:
+                slow+=1
+                nums[slow]=nums[fast]
                
-        
-     
-
-
-        
+        return slow+1    #because we add 1 we start slow from 0 that is why    
+          
         
