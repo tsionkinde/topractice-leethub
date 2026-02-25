6204bb7d1c824a36f756166ffaa3d06c01f22b44
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-02-25 02:23:27 -0800

    Time: 48 ms (76.22%), Space: 25.1 MB (64.21%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,26 +1,19 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people.sort(reverse=True)# the boat can carry at most two peoples regardless of the limit 
-        left=0
-        right=len(people)-1
-        boats=0
-        while left <= right:
-            if people[left] + people[right] <= limit:
-                left += 1
-                right -= 1
-            else:
-                left += 1
-            boats += 1
-        return boats
-        
-          
+        boats=0       
+        people.sort()
+        l=0
+        r=len(people)-1
 
-
-                
+        while l<=r:
+            if people[l]+people[r]<=limit:
+                l+=1
+            r-=1
+            boats+=1
+         
+        return boats            
 
 
 
-
-                
-
+            
         
