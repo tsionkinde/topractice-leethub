7f71293dce32fddeacea5860734cce406fbe226e
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-03-01 12:50:27 -0800

    Time: 0 ms (100%), Space: 19.3 MB (62.94%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        
+      
+        for layer in range(n // 2):
+            first = layer
+            last = n - 1 - layer
+            for i in range(first, last):
+                offset = i - first
+                
+           
+                top = matrix[first][i]
+                
+              
+                matrix[first][i] = matrix[last - offset][first]
+                
+              
+                matrix[last - offset][first] = matrix[last][last - offset]
+            
+                matrix[last][last - offset] = matrix[i][last]
+                
+       
+                matrix[i][last] = top
+        
