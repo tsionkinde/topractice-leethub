9fb4348df747b303c264c4c9e463cda621e4a1f6
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-03-09 08:50:32 -0700

    Time: 1 ms (20.98%), Space: 19.3 MB (77.89%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count=0
+        for log in logs:
+            if log=="./":
+                continue
+            elif log=="../":
+                if count>0:
+                    count-=1
+            else:
+                count+=1
+        return count                    
+
+        
+        
