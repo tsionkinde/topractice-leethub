3d4aa31e29020992b5a758498c4368dee3980fee
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-03-23 10:03:58 -0700

    Time: 0 ms (100%), Space: 19.2 MB (88.01%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        moves=0
+        while target>1 and maxDoubles>0:
+            if target%2==0:
+                target//=2
+                maxDoubles-=1
+            else:
+                target-=1
+            moves+=1
+        moves+=target-1
+        return moves        
+
+        
