b5694b594a8f2e62b3dc5831f19d11e3c7995cda
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-03-28 10:57:25 +0300

    Time: 0 ms (100%), Space: 19.4 MB (50.72%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,7 +2,7 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res=[]
         def backtrack(path,index):
-            res.append(path[:])
+            res.append(path[:])#to copy the current subset
             for i in range(index,len(nums)):
                 path.append(nums[i])
                 backtrack(path,i+1)
