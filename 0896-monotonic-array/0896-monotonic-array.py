0c0dbdf4bc5847e2bf65bd6d528da1cf64c7f044
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-04-08 18:27:17 +0300

    Time: 94 ms (7.79%), Space: 31.6 MB (48.22%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if nums==sorted(nums) or nums == sorted(nums,reverse=True):
+            return True
+        else:
+            return False    
+        
