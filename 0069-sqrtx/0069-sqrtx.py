e91f015a7e53430a8cc971463dbd1ed07da47052
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-05-01 19:41:35 +0300

    Time: 0 ms (100%), Space: 19.4 MB (6.91%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,6 +1,18 @@
-import math
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(math.sqrt(x))
+        if x==0:
+            return 0
+        left,right=1,x
+        res=0
+        while left<=right:
+            mid=(left+right)//2
+            if mid <= x//mid:
+                res=mid
+                left=mid+1
+            else:
+                right=mid-1
+        return res        
 
-       
+
+
+        
