33ed9c9828365cd5b3c931f141b60169f433eb1e
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-05-18 00:05:42 -0700

    Time: 2 ms (7.3%), Space: 19.4 MB (6.08%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
@@ -1,19 +1,11 @@
 class Solution:
     def sumBase(self, n: int, k: int) -> int:
-        def to_base_k(n, k):
-            if n == 0:
-                return "0"
-            digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            res = ""
-            while n > 0:
-                res = digits[n % k] + res
-                n //= k
-            return res    
-        res=to_base_k(n, k)        
-        x=[]    
-        for i in res:
-            x.append(int(i))
-        return sum(x)    
+        total=0
+        while n>0:
+            total += n%k
+            n//=k
+        return total    
+            
                 
 
                 
