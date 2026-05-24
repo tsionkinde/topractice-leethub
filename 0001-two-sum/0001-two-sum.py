c605b92ba5bc36ebe3ef8dda09c4b04d1f4882b1
tsionkinde <tsion.ugr-2656-16@aau.edu.et>
2026-05-24 13:15:09 -0700

    Time: 1948 ms (7.4%), Space: 19.8 MB (74.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-      num_to_index={}
-      for index,num in enumerate(nums):
-        if target-num in num_to_index:
-            return [num_to_index[target-num],index]
-        num_to_index[num]=index
-      return []       
-            
+        the_index=[]
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                     the_index.append(i)#the 2 lines of append are in two d/t lines b.c append does not take  two arguments
+                     the_index.append(j)
+        return the_index
+            # we can also code like this b/c the above one is by using brute force
+            #hm={}
+            #for i,v in enumerate(nums):
+                #if hm and target -v in hm:
+                    #return [hm[target-v],i]
+                #hm[v]=i    
         
